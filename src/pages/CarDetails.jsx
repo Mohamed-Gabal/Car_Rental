a6c74f4b2e75eddb93672bd0ded0d10524805271
Mohamed Gabal <abogabal672@gmail.com>
2026-01-31 17:42:38 +0200

    finished from pages Home and Cars

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -7,16 +7,24 @@ import Gasoline from "../assets/gasoline.svg";
 import Automatic from "../assets/automatic.svg";
 import Location from "../assets/location_icon.svg";
 import Check from "../assets/chech.svg";
+import Loader from "../components/Loader";
 
 const CarDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [car, setCar] = useState();
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  // HANDLE FROM SUBMIT
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
 
   // useEffect();
   useEffect(() => {
     setCar(dummyCarData.find((car) => car._id === id));
   }, [id]);
+
   return car ? (
     <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-16">
       <button
@@ -78,25 +86,65 @@ const CarDetails = () => {
             <div>
               <h1 className="text-xl font-medium mb-3">Features</h1>
               <ul className="grid grid-cols-1 sm:grid-cols-2 gap-2">
-                {
-                  ["360 Camera", "Bluetooth", "GPS", "Heated Seats", "Rear View Mirror"].map((item) => (
-                    <li key={item} className="flex items-center text-gray-500">
-                      <img src={Check} alt="check" className="h-4 mr-2" />
-                      {item}
-                    </li>
-                  ))
-                }
+                {[
+                  "360 Camera",
+                  "Bluetooth",
+                  "GPS",
+                  "Heated Seats",
+                  "Rear View Mirror",
+                ].map((item) => (
+                  <li key={item} className="flex items-center text-gray-500">
+                    <img src={Check} alt="check" className="h-4 mr-2" />
+                    {item}
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
         </div>
 
         {/* RIGHT: BOOKING FORM */}
-        <div className=""></div>
+        <form onSubmit={handleSubmit} className="shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500">
+          <p className="flex items-center justify-between text-2xl text-gray-800 font-semibold">
+            {currency} {car.pricePerDay}
+            <span className="text-base text-gray-400 font-normal">per day</span>
+          </p>
+          <hr className="border-borderColor my-6" />
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="pickup-data">Pickup Data</label>
+            <input
+              type="date"
+              className="border border-borderColor px-3 py-2 rounded-lg "
+              required
+              id="pickup-data"
+              min={new Date().toISOString().split("T")[0]}
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="return-data">Return Data</label>
+            <input
+              type="date"
+              className="border border-borderColor px-3 py-2 rounded-lg "
+              required
+              id="return-data"
+            />
+          </div>
+
+          <button className="w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer">
+            Book Now
+          </button>
+
+          <p className="text-center text-sm">
+            No credit card required to reserve
+          </p>
+        </form>
       </div>
     </div>
   ) : (
-    <p>Loading...</p>
+    // IMPORT LOADER COMPONENT
+    <Loader />
   );
 };
 
--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Title from "../components/Title";
+import { dummyCarData } from "../assets/assets";
 
 const MyBookings = () => {
+  const [bookings, setBookings] = useState([]);
+
+  const fetchMyBookings = async () => {
+    // LOGIC TO FETCH BOOKINGS FROM API OR STATE MANAGEMENT
+    setBookings(dummyCarData);
+  };
+
+  useEffect(() => {
+    fetchMyBookings();
+  }, []);
+
   return (
-    <div className=''></div>
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
+      {/* IMPORT TITLE COMPONENT */}
+      <Title
+        title="My Bookings"
+        subTitle="View and manage your car bookings"
+        align="left"
+      />
+
+      <div>
+        {bookings.map((booking, index) => (
+          <div
+            key={booking._id}
+            className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6 border border-borderColor rounded-lg mt-5 first:mt-12"
+          >
+            {/* CAR INAGE + INFO */}
+            <div className="md:col-span-1">
+              <div className="rounded-md overflow-hidden mb-3">
+                <img
+                  src={booking.image}
+                  alt={booking.name}
+                  className="w-full h-auto"
+                />
+              </div>
+              <p className="text-lg font-medium mt-2">
+                {booking.brand} {booking.model}
+              </p>
+              <p className="text-gray-500">
+                {booking.year} {booking.category} {booking.location}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
-}
+};
 
 export default MyBookings;
